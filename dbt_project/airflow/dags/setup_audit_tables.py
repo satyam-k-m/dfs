d76f956bf26f21e32d00df821af7ac77d6a32680
Ubuntu <satyam@ubuntu.thbnzb0d3dmuziyfg2t53bofqb.tx.internal.cloudapp.net>
2023-07-05 20:42:31 +0000

    updating dag

--- dbt_project/airflow/dags/setup_audit_tables.py
+++ dbt_project/airflow/dags/setup_audit_tables.py
@@ -6,8 +6,7 @@ from airflow.operators.bash import BashOperator
 from airflow.decorators import dag, task, task_group
 from airflow.utils.task_group import TaskGroup
 from airflow.providers.microsoft.azure.sensors.wasb import WasbBlobSensor
-from test_task_builder import *
-from dbt_project.airflow.dags.task_builder import *
+from task_builder import *
 from airflow.decorators import dag
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
 from pendulum import datetime
