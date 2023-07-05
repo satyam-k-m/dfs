ec6365c864a56082cc8b8ee8ce64cd79859083f5
Ubuntu <satyam@ubuntu.thbnzb0d3dmuziyfg2t53bofqb.tx.internal.cloudapp.net>
2023-07-05 21:38:57 +0000

    adding audit

--- dbt_project/airflow/dags/task_builder.py
+++ dbt_project/airflow/dags/task_builder.py
@@ -18,18 +18,13 @@ class TaskBuilder():
         self.dbt_path = dbt_path
 
 
+
     def build_source_task_group(self, node_info):
         source_id = node_info["depends_on"]["nodes"][0]
         item = self.sources[source_id]
         group_id = source_id.replace(".", "_")
-        group_id_name = f"{group_id}_source"
-
-        with TaskGroup(group_id=group_id_name) as tg:
-
-            # insert_task_dim = BaseException(
-            #     task_id="dummy_bash_operator_pre",
-            #     bash_command= "echo Hello")
-
+        
+        with TaskGroup(group_id=group_id) as tg:
 
             insert_dim_task_source =  SnowflakeOperator(
                     task_id = "insert_dim_task_source",
@@ -47,24 +42,23 @@ class TaskBuilder():
                         ]
                     ),
                 bash_command=f"cd {self.dbt_path}" # Go to the path containing your dbt project
-                + f" && dbt run --models {node_info['name']}", # run the model!
+                + f" && dbt build --models {node_info['name']} ", # run the model!
                 )
 
-            insert_dim_task_source >> source_id 
+            
+            insert_dim_task_source >> source_id
             
         return tg
     
     def build_node_task_group(self,node_id, node_info):
-
         group_id = node_id.replace(".", "_")
         with TaskGroup(group_id=group_id) as tg:
-            
+
             insert_dim_task_model =  SnowflakeOperator(
                     task_id = "insert_dim_task_model",
                     sql = sql_stmts.insert_dim_task,
                     params = {"table_name":SNOWFLAKE_DIM_TASK_TABLE, "pipeline_id": tg.dag_id, "task_name":group_id}
-            )
-            
+                )
             node_id = BashOperator(
                 task_id= ".".join(
                     [
@@ -75,12 +69,86 @@ class TaskBuilder():
                 ),
 
                 bash_command=f"cd {self.dbt_path}" # Go to the path containing your dbt project
-                + f" && dbt run --models {node_info['name']}", # run the model!
+                + f" && dbt build --models {node_info['name']} ", # run the model!
             )
-
-            insert_dim_task_model >> node_id
+           
+            insert_dim_task_model >> node_id 
             
         return tg
+         
+
+
+# class TaskBuilder():
+#     def __init__(self, nodes, sources, dbt_path):
+#         self.nodes = nodes
+#         self.sources = sources
+#         self.dbt_path = dbt_path
+
+
+#     def build_source_task_group(self, node_info):
+#         source_id = node_info["depends_on"]["nodes"][0]
+#         item = self.sources[source_id]
+#         group_id = source_id.replace(".", "_")
+#         group_id_name = f"{group_id}_source"
+
+#         with TaskGroup(group_id=group_id_name) as tg:
+
+#             # insert_task_dim = BaseException(
+#             #     task_id="dummy_bash_operator_pre",
+#             #     bash_command= "echo Hello")
+
+
+#             insert_dim_task_source =  SnowflakeOperator(
+#                     task_id = "insert_dim_task_source",
+#                     sql = sql_stmts.insert_dim_task,
+#                     params = {"table_name":SNOWFLAKE_DIM_TASK_TABLE, "pipeline_id": tg.dag_id, "task_name":group_id}
+#                 )
+            
+#             source_id = BashOperator(
+#                     task_id = ".".join(
+#                         [
+#                             item["resource_type"],
+#                             item["package_name"],
+#                             item['source_name'],
+#                             item["name"],
+#                         ]
+#                     ),
+#                 bash_command=f"cd {self.dbt_path}" # Go to the path containing your dbt project
+#                 + f" && dbt run --models {node_info['name']}", # run the model!
+#                 )
+
+#             insert_dim_task_source >> source_id 
+            
+#         return tg
+    
+#     def build_node_task_group(self,node_id, node_info):
+
+#         group_id = node_id.replace(".", "_")
+#         group_name = f"{group_id}_model"
+#         with TaskGroup(group_id=group_name) as tg:
+            
+#             insert_dim_task_model =  SnowflakeOperator(
+#                     task_id = "insert_dim_task_model",
+#                     sql = sql_stmts.insert_dim_task,
+#                     params = {"table_name":SNOWFLAKE_DIM_TASK_TABLE, "pipeline_id": tg.dag_id, "task_name":group_id}
+#             )
+            
+#             node_id = BashOperator(
+#                 task_id= ".".join(
+#                     [
+#                         node_info["resource_type"],
+#                         node_info["package_name"],
+#                         node_info["name"],
+#                     ]
+#                 ),
+
+#                 bash_command=f"cd {self.dbt_path}" # Go to the path containing your dbt project
+#                 + f" && dbt run --models {node_info['name']}", # run the model!
+#             )
+
+#             insert_dim_task_model >> node_id
+            
+#         return tg
          
 
 
