f30abb0ae405a0d85877585579c933a5bce4da78
Ubuntu <satyam@ubuntu.thbnzb0d3dmuziyfg2t53bofqb.tx.internal.cloudapp.net>
2023-07-05 20:20:47 +0000

    updating model ref

--- dbt_project/dbt/dbt/models/bronze/bronze_rfnd_orgnl_tx_rfrnc.sql
+++ dbt_project/dbt/dbt/models/bronze/bronze_rfnd_orgnl_tx_rfrnc.sql
@@ -9,24 +9,24 @@
 
 with RFND_ORGNL_TX_RFRNC as (
      select
-		ORIG_TX_NBR NUMBER(38,0) AS (value:c1::NUMBER(38,0)),
-		ORIG_TERM_NBR NUMBER(38,0) AS (value:c2::NUMBER(38,0)),
-		ORIG_DIV_NBR NUMBER(38,0) AS (value:c3::NUMBER(38,0)),
-		ORIG_POS_LOC_ID NUMBER(38,0) AS (value:c4::NUMBER(38,0)),
-		ORIG_BIZ_DT NUMBER(38,0) AS (value:c5::NUMBER(38,0)),
-		ORIG_ADJ_FLG VARCHAR(1) AS (value:c6::varchar(1)),
-		ORIG_TX_DT NUMBER(38,0) AS (value:c7::NUMBER(38,0)),
-		RFND_TX_NBR NUMBER(38,0) AS (value:c8::NUMBER(38,0)),
-		RFND_TERM_NBR NUMBER(38,0) AS (value:c9::NUMBER(38,0)),
-		RFND_DIV_NBR NUMBER(38,0) AS (value:c10::NUMBER(38,0)),
-		RFND_POS_LOC_ID NUMBER(38,0) AS (value:c11::NUMBER(38,0)),
-		RFND_BIZ_DT NUMBER(38,0) AS (value:c12::NUMBER(38,0)),
-		RFND_ADJ_FLG VARCHAR(1) AS (value:c13::varchar(1)),
-		RFND_TX_DT NUMBER(38,0) AS (value:c14::NUMBER(38,0)),
+		value:c1::NUMBER(38,0) AS ORIG_TX_NBR,
+        value:c2::NUMBER(38,0) AS ORIG_TERM_NBR,
+        value:c3::NUMBER(38,0) AS ORIG_DIV_NBR,
+        value:c4::NUMBER(38,0) AS ORIG_POS_LOC_ID,
+        value:c5::NUMBER(38,0) AS ORIG_BIZ_DT,
+        value:c6::varchar(1) AS ORIG_ADJ_FLG,
+        value:c7::NUMBER(38,0) AS ORIG_TX_DT,
+        value:c8::NUMBER(38,0) AS RFND_TX_NBR,
+        value:c9::NUMBER(38,0) AS RFND_TERM_NBR,
+        value:c10::NUMBER(38,0) AS RFND_DIV_NBR,
+        value:c11::NUMBER(38,0) AS RFND_POS_LOC_ID ,
+        value:c12::NUMBER(38,0) AS RFND_BIZ_DT,
+        value:c13::varchar(1) AS RFND_ADJ_FLG,
+        value:c14::NUMBER(38,0) AS RFND_TX_DT,
         division,
         run_dt,
         SHA2_HEX(concat_ws('~',ORIG_TX_NBR,ORIG_TERM_NBR)) as surr_key,
-        to_timestamp('{{ var("run_ts") }}') as run_ts  from {{source('dfs_stage','ext_rfnd_orgnl_tx_rfrnc') }}
+        to_timestamp('{{ var("run_ts") }}') as run_ts  from {{source('dfs_stage','ext_rfnd_tx_rf') }}
 )
 
 select * from RFND_ORGNL_TX_RFRNC
