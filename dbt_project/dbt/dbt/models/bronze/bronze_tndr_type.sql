1cb9d87ada04bc64ad3e35e38683e2ae0b5845c0
Ubuntu <satyam@ubuntu.thbnzb0d3dmuziyfg2t53bofqb.tx.internal.cloudapp.net>
2023-07-05 20:36:24 +0000

    updating ref

--- dbt_project/dbt/dbt/models/bronze/bronze_tndr_type.sql
+++ dbt_project/dbt/dbt/models/bronze/bronze_tndr_type.sql
@@ -9,12 +9,12 @@
 
 with TNDR_TYPE as (
      select
-		TEN_TYPE NUMBER(38,0) AS (value:c1::NUMBER(38,0)),
-		DIV_NBR NUMBER(38,0) AS (value:c2::NUMBER(38,0)),
-		SHORT_DESC VARCHAR(4) AS (value:c3::varchar(4)),
-		LONG_DESC VARCHAR(30) AS (value:c4::varchar(30)),
-		CURR_CARD_TYPE VARCHAR(1) AS (value:c5::varchar(1)),
-		SRC_UPDT_DT NUMBER(38,0) AS (value:c6::NUMBER(38,0)),
+		value:c1::NUMBER(38,0) AS TEN_TYPE),
+        value:c2::NUMBER(38,0) AS DIV_NBR,
+        value:c3::varchar(4) AS SHORT_DESC,
+        value:c4::varchar(30) AS LONG_DESC,
+        value:c5::varchar(1) AS CURR_CARD_TYPE,
+        value:c6::NUMBER(38,0) AS SRC_UPDT_DT,
         division,
         run_dt,
         SHA2_HEX(concat_ws('~',ten_type,div_nbr)) as surr_key,
--- dbt_project/dbt/dbt/models/bronze/bronze_tx_type.sql
+++ dbt_project/dbt/dbt/models/bronze/bronze_tx_type.sql
@@ -9,10 +9,10 @@
 
 with TX_TYPE as (
      select
-		DIV_NBR NUMBER(38,0) AS (value:c1::NUMBER(38,0)),
-		APP_CD VARCHAR(2) AS (value:c2::varchar(2)),
-		TX_TYPE VARCHAR(2) AS (value:c3::varchar(2)),
-		SHORT_DESC VARCHAR(30) AS (value:c4::varchar(30)),
+		value:c1::NUMBER(38,0) AS DIV_NBR,
+        value:c2::varchar(2) AS APP_CD,
+        value:c3::varchar(2) AS TX_TYPE,
+        value:c4::varchar(30) AS SHORT_DESC,
         division,
         run_dt,
         SHA2_HEX(concat_ws('~',div_nbr,app_cd,tx_type)) as surr_key,
